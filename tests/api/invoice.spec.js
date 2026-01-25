4dc432658f82d57b2435cded1f843d15722ccecd
Baburao Adkane <b.g.adkane27@gmail.com>
2026-01-25 15:47:07 +0530

    Token Added

--- tests/api/invoice.spec.js
+++ tests/api/invoice.spec.js
@@ -2,15 +2,12 @@ import { test, expect } from '@playwright/test';
 import InvoiceController from '../../lib/api/InvoiceController.js';
 import invoicePayload from '../../test-data/invoice.payload.json';
 
-test('Create Invoice API - success', async () => {
+test('Create Invoice - success', async () => {
     const invoiceApi = new InvoiceController();
 
     const response = await invoiceApi.createInvoice(invoicePayload);
 
-    expect(response.status()).toBe(200);
-
-    const body = await response.json();
-    expect(body.Success).toBe(true);
+    expect(response.status()).toBe(200);    
 
     await invoiceApi.dispose();
 });
