e1acf3d8c44ca9a1096327b4bd074702989b95ae
Baburao Adkane <b.g.adkane27@gmail.com>
2026-01-30 15:10:49 +0530

    Get all items test added

--- tests/api/item.spec.js
+++ tests/api/item.spec.js
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test';
 import ItemController from '../../lib/api/ItemController.js';
 import { itemPayload } from '../../test-data/item.payload.js';
 
-test.only('Create Item - Success', async () => {
+test('Create Item - Success', async () => {
     const itemApi = new ItemController();
 
-    const payload = itemPayload();    
+    const payload = itemPayload();
     const response = await itemApi.createItem(payload);
 
     if (!response.ok()) {
@@ -15,4 +15,22 @@ test.only('Create Item - Success', async () => {
     expect(response.status()).toBe(200);
 
     await itemApi.dispose();
+});
+
+test.only('Get All Items', async () => {
+    const itemApi = new ItemController();
+
+    const response = await itemApi.getAllItems('2026-01-26T09:00:00', 0, 100);
+
+    if (!response.ok()) {
+        console.error('Get All Items failed:', await response.text());
+    }
+
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+    expect(body.data.length).toBeGreaterThan(0);
+
+    await itemApi.dispose();
+
 });
